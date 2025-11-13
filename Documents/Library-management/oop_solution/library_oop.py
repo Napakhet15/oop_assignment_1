29eb8cf8bdeaf2fb2c2e9138a3a687b474808194
Napakhet <naphaketjnnn@gmail.com>
2025-11-13 21:31:54 +0700

    Add Member class

--- Documents/Library-management/oop_solution/library_oop.py
+++ Documents/Library-management/oop_solution/library_oop.py
@@ -25,3 +25,55 @@ class Book:
     def __str__(self):
         return f"[{self.id}] {self.title} by {self.author} ({self.available_copies}/{self.total_copy} available)"
 
+class Member:
+    def __init__(self, id, name, email):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.borrowed_books_list = []
+
+    def can_borrow(self):
+        """Check member borrow books"""
+        return len(self.borrowed_books_list) < 3
+
+    def borrow_book(self, book):
+        """Borrow a book if allowed"""
+        if book.id in self.borrowed_books_list:
+            print("Error: Member already borrowed this book!")
+            return False
+
+        if not self.can_borrow():
+            print("Error: Member has reached the borrowing limit!")
+            return False
+
+        if not book.borrow():
+            print("Error: No copies available!")
+            return False
+
+        self.borrowed_books_list.append(book.id)
+        print(f"{self.name} borrowed '{book.title}'")
+        return True
+
+    def return_book(self, book):
+        """Return borrow book"""
+        if book.id not in self.borrowed_books_list:
+            print("Error: This member hasn't borrowed this book!")
+            return False
+
+        book.return_book()
+        self.borrowed_books_list.remove(book.id)
+        print(f"{self.name} returned '{book.title}'")
+        return True
+
+    def __str__(self):
+        """Display member borrow book """
+        if not self.borrowed_books_list:
+            borrowed = "No books borrowed"
+        else:
+            borrowed = ""
+            for book_id in self.borrowed_books_list:
+                borrowed += f"{book_id}, "
+            borrowed = borrowed.rstrip(", ")
+        return f"Member: {self.name} ({self.email}) | Borrowed: {borrowed}"
+
+
